d7c4aaa9a8d1322ee0468af242acb37c4cbab5cd
henrikci <34025697+henrikci@users.noreply.github.com>
2018-02-07 15:42:58 +0200

    Add files via upload

---
+++ Bulli/BallController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D collision){
+	
+//		Debug.Log (collision.gameObject);
+//		collision.gameObject.transform.Translate (0, 0, 0);
+		transform.Translate (0, 0.1f, 0);
+	}
+	
+}
