66e9d1cd6cae8ac8cf414886c5bbb9d1877a3088
henrikci <34025697+henrikci@users.noreply.github.com>
2018-02-12 11:49:48 +0200

    Add files via upload
    Uusimmat siivotut versiot (12.2.2018)

---
+++ Bulli/BullController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BullController : MonoBehaviour {
+	//instanssimuuttujat
+	private float bullSpeed = 8;
+	private Rigidbody2D bull;
+
+	//törmäys ja hypyn nollaaminen
+	void OnCollisionEnter2D(Collision2D collision){
+		transform.Translate (0, 0.1f, 0);
+		isJumping = false;
+	}
+
+	void Start(){
+		bull = GetComponent<Rigidbody2D>();
+	}
+
+	void Update(){
+		//liikkuminen
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			bull.transform.Translate (-0.5f * bullSpeed, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			bull.transform.Translate (0.5f * bullSpeed, 0, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Pomppaus ();
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			bull.transform.Translate (0, -0.00000001f * bullSpeed, 0);
+		}
+	}
+
+	//hyppiminen
+	private Vector2 jumppi = new Vector2 (0f, 60f);
+	private bool isJumping = false;
+
+	public void Pomppaus ()
+	{
+
+		if (Input.GetKeyDown (KeyCode.Space) && isJumping == false) {
+			bull.AddForce (5 * jumppi, ForceMode2D.Impulse);
+			isJumping = true; 
+		} 
+	}
+
+}
+
+
---
+++ Bulli/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	//instanssimuuttujat
+	public Transform target;
+	private float cameraSpeed = 15f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//kameran seuraaminen
+		Vector3 newPosition = new Vector3(target.position.x, transform.position.y,transform.position.z);
+		transform.position = Vector3.Lerp (transform.position, newPosition, cameraSpeed * Time.deltaTime);
+	}
+}
