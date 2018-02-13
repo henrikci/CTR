078c7e7faf65df03ae58be8d96d018280e65f6b5
henrikci <34025697+henrikci@users.noreply.github.com>
2018-02-13 11:44:53 +0200

    Add files via upload
    Uusimmat versioooonssit boi

--- Bulli/BullController.cs
+++ Bulli/BullController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BullController : MonoBehaviour {
 	//instanssimuuttujat
-	private float bullSpeed = 8;
+	private float bullSpeed = 12;
 	private Rigidbody2D bull;
+	private float maxSpeed = 180f;
+	private Vector2 vertical = new Vector2 (0.4f,0f);
 
 	//törmäys ja hypyn nollaaminen
 	void OnCollisionEnter2D(Collision2D collision){
@@ -18,34 +21,47 @@ public class BullController : MonoBehaviour {
 	}
 
 	void Update(){
+
+		if(bull.velocity.magnitude > maxSpeed)
+		{
+			bull.velocity = bull.velocity.normalized * maxSpeed;
+		}
+
 		//liikkuminen
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			bull.transform.Translate (-0.5f * bullSpeed, 0, 0);
+			bull.AddForce (-18 * vertical, ForceMode2D.Impulse);
 		}
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			bull.transform.Translate (0.5f * bullSpeed, 0, 0);
+			
+			bull.AddForce(18 * vertical, ForceMode2D.Impulse);
 		}
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			Pomppaus ();
 		}
 		if (Input.GetKey (KeyCode.DownArrow)) {
-			bull.transform.Translate (0, -0.00000001f * bullSpeed, 0);
+			bull.transform.Translate (0, -0.001f * bullSpeed, 0);
 		}
 	}
 
 	//hyppiminen
-	private Vector2 jumppi = new Vector2 (0f, 60f);
+	private Vector2 jumppi = new Vector2 (0f, 10f);
 	private bool isJumping = false;
 
 	public void Pomppaus ()
 	{
 
 		if (Input.GetKeyDown (KeyCode.Space) && isJumping == false) {
-			bull.AddForce (5 * jumppi, ForceMode2D.Impulse);
+			bull.AddForce (14 * jumppi, ForceMode2D.Impulse);
 			isJumping = true; 
 		} 
 	}
 
+ 
+//	public void OnTriggerEnter (Collider player, Collider levelEnd)
+//	{
+//		if (player.tag == "Player" && levelEnd.tag == "LevelEnd") {
+//			Application.LoadLevel ("levelSelectLevelVantaaOpened");
+//	}
+//}
 }
-
 
--- Bulli/ButtonController.cs
+++ Bulli/ButtonController.cs
@@ -6,16 +6,16 @@ using UnityEngine.EventSystems;
 public class ButtonController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
 
 	private bool buttonPressed = false;
+
 	public void OnPointerDown(PointerEventData e){
 		buttonPressed = true;
 	}
+
 	public bool getButtonPressed(){
 		return buttonPressed;
 	}
 
 	public void OnPointerUp(PointerEventData e){
 		buttonPressed = false;
-
 	}
-
 }
